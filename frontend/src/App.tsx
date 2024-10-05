eddd83889c5adc9b4cf721c61936657aa0bbab8d
mattheww02 <78615724+mattheww02@users.noreply.github.com>
2024-10-05 01:12:07 +0100

    Added Bootstrap CSS template

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import icon from './assets/img/ib_icon.png';
 import './App.css';
+import './css/styles.css';
+import NavBar from './components/NavBar';
 import Home from './pages/Home';
 import Board from './pages/Board';
 
 function App() {
+  useEffect(() => {
+    const link = document.createElement('link');
+    link.href = 'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@100;200;300;400;500;600;700;800;900&display=swap';
+    link.rel = 'stylesheet';
+    document.head.appendChild(link);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={icon} className="App-logo" alt="logo" />
-        <Home/>
-        <Board/>
-      </header>
+      <NavBar/>
+      <img src={icon} className="App-logo" alt="logo" />
+      <Home/>
+      <Board/>
     </div>
   );
 }
