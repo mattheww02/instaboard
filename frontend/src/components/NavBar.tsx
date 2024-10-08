1667a1470880ff36357a40eff8754561400506c3
mattheww02 <78615724+mattheww02@users.noreply.github.com>
2024-10-08 13:48:36 +0100

    Added About page

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -12,9 +12,9 @@ const NavBar = () => {
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav ms-auto mb-2 mb-lg-0 small fw-bolder">
                         <li className="nav-item"><a className="nav-link" href="/">Home</a></li>
-                        <li className="nav-item"><a className="nav-link" href="/">Profile</a></li>
-                        <li className="nav-item"><a className="nav-link" href="/">Boards</a></li>
-                        <li className="nav-item"><a className="nav-link" href="/">About</a></li>
+                        {/* <li className="nav-item"><a className="nav-link" href="/">Profile</a></li> */}
+                        {/* <li className="nav-item"><a className="nav-link" href="/">Boards</a></li> */}
+                        <li className="nav-item"><a className="nav-link" href="/about">About</a></li>
                     </ul>
                 </div>
             </div>
---
+++ frontend/src/pages/About.tsx
@@ -0,0 +1,18 @@
+
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <section className="py-5 bg-gradient-primary-to-secondary text-white">
+                <div className="container px-5 my-5">
+                    <div className="text-center">
+                        <h2 className="display-4 fw-bolder mb-4">About</h2>
+                        <a className="btn btn-outline-light btn-lg px-5 py-3 fs-6 fw-bolder" href="/">Go back</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default About;
